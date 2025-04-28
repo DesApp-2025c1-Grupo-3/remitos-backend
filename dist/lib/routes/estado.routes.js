f7cef64734b137b04cf77f3b1c5f4c33f39bbf1d
Nicolas Benoit <123339751+Nikandro1@users.noreply.github.com>
2025-04-27 21:26:12 -0300

    agregando controllers, routes, middleware y schemas de contacto y cliente

--- dist/lib/routes/estado.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express"); // Creamos el router
-
-const route = express.Router(); // corregido a require también si era import antes
-// Exportás el router
-
-module.exports = route;
-//# sourceMappingURL=estado.routes.js.map
